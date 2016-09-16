8e74775de43605d8e85e40c74a3dd8010b0590bc
Christina Jeong <fae.ah.fae@gmail.com>
2016-09-16 10:04:13 +0900

    get JSON object in from NYT and then access it with array "response"

--- Apps/nyt_topstories_v2.js
+++ Apps/nyt_topstories_v2.js
@@ -1,3 +1,16 @@
-/**
- * Created by Jiyong on 15-Sep-16.
- */
+var response = [];
+var url = "https://api.nytimes.com/svc/topstories/v2/world.json";
+url += '?' + $.param({
+        'api-key': "591e19bb7d974693b30e645f3288102d"
+    });
+$.ajax({
+    url: url,
+    method: 'GET',
+}).done(function(result) {
+    for (i = 0; i < 10; i++) {
+        var x = JSON.parse(JSON.stringify(result.results[i]));
+        response.push(x);
+    }
+}).fail(function(err) {
+    throw err;
+});
