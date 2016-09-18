097effa19f2a592656c58ef103af2c82cd4e86b5
Christina Jeong <fae.ah.fae@gmail.com>
2016-09-19 00:38:41 +0900

    Add jquery-ui css link; try to make panel more pretty... it starts off not selected, has article pic links in contents panel

--- Apps/pins.js
+++ Apps/pins.js
@@ -13,12 +13,11 @@ function getLocationFromBingAPI(location, name) {
     });
 };
 
-// this will hold all the coordinates (longitude,latitude) of the articles in 2D array
-var coord = [];
+var coord = []; // this will hold all the coordinates (longitude,latitude) of the articles in 2D array
+var pin = []; // store all pins in this array
 
 // first loop to find coordinates for the articles first
 setTimeout(function(){
-    //console.log(response); // print out the results from NYT in browser console
     for (i = 0; i < 10; i++) {
         coord[i] = [];
         getLocationFromBingAPI(coord[i], response[i.toString()].geo_facet["0"]);
@@ -27,10 +26,9 @@ setTimeout(function(){
 
 // second loop to put pins on map
 setTimeout(function(){
-    console.log(coord); // print out the results of all the coordinates
     for (i = 0; i < 10; i++) {
         var j = i.toString();
-        var pin = viewer.entities.add({
+        pin[i] = viewer.entities.add({
             name: response[j].title,
             position: Cesium.Cartesian3.fromDegrees(coord[i][0], coord[i][1]),
             billboard: {
@@ -43,7 +41,11 @@ setTimeout(function(){
 
         // populate accordion menu
         var newstitle = (i+1) + ". " + response[j].title;
-        var newsabstract = response[j].abstract;
+        var newsabstract = "";
+        if (response[j].multimedia.length != 0) {
+            newsabstract += '<img src=\"'+response[j].multimedia["0"].url+'\" align=\"right\" style=\"padding-left: 5px\">';
+        }
+        newsabstract += response[j].abstract;
         $("#news"+i+"title").html(newstitle);
         $("#news"+i+"abstract").html(newsabstract);
 
@@ -52,3 +54,10 @@ setTimeout(function(){
         console.log(coord[i][0]+ " "+coord[i][1]);
     }
 }, 2000); // delay function by 2000ms to get loc info from Bing
+
+// testing function to see if everything is working as expected -- prints in browser console
+setTimeout(function() {
+    console.log(response); // print out the results from NYT in browser console
+    console.log(coord); // print out the results of all the coordinates
+    console.log(pin);
+}, 5000);
